a6e498357200a685a3c7d4eaec7aa6c96ebb2166
asela1982 <asela.d82@gmail.com>
2018-03-31 11:06:24 -0500

    add models

---
+++ ScrapeData/mongodb_scripts/mongodb_import.sh
@@ -0,0 +1,1 @@
+./mongoimport -d master_chef -c recipes --type csv --file recipe_df4.csv --headerline
---
+++ ScrapeData/mongodb_scripts/mongodb_restore.sh
@@ -0,0 +1,2 @@
+./mongorestore --collection recipes --db heroku_kp3hp48w --uri mongodb://heroku_kp3hp48w:[PASSWORD]@ds153958.mlab.com:53958 dump/master_chef/recipes.bson 
+
